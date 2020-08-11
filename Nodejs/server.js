16e01fb906ec63079c29434d26bed4e1a31b5a85
DacPhuc <phuc.nguyendac@hcmut.edu.vn>
2020-08-11 14:04:44 +0700

    Change from typescript to js

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -8,8 +8,8 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/api/index", (req, res) => {
+  res.send("Hello World, this data is send from express server");
 });
 
 app.listen(PORT, HOST);
